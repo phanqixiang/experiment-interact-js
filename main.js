ec62ae5c0ae79e92ec9c30e3ffe510e021bc8651
Phan Qi Xiang <phanqixiang85@gmail.com>
2024-09-09 17:07:54 +0800

    Change transform approach to transform instead positioning

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
 import './style.css'
 import interact from 'interactjs'
 
-const position = { x: 0, y: 0 } 
-interact('#transform-image')
+interact('.resize-drag')
   .resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: true, top: true },
+    listeners: {
+      move (event) {
+        var target = event.target
+
+        if(event.rect.width < 200){
+          return 
+        }
+        var x = (parseFloat(target.getAttribute('data-x')) || 0)
+        var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+        // update the element's style
+        target.style.width = event.rect.width + 'px'
+        target.style.height = event.rect.height + 'px'
+
+        // translate when resizing from top or left edges
+        x += event.deltaRect.left
+        y += event.deltaRect.top
+
+        target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+
+        target.setAttribute('data-x', x)
+        target.setAttribute('data-y', y)
+        // target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+      }
+    },
     modifiers: [
+      // keep the edges inside the parent
+      interact.modifiers.restrictEdges({
+        outer: 'parent'
+      }),
       interact.modifiers.aspectRatio({
         // make sure the width is always double the height
         ratio: 'preserve',
@@ -14,35 +44,29 @@ interact('#transform-image')
         ],
       }),
     ],
-    edges: { top: true, left: true, bottom: true, right: true },
-    listeners: {
-      move: function (event) {
-        const currentTop = window.getComputedStyle(event.target).top.replace("px", "")
-        const currentLeft = window.getComputedStyle(event.target).left.replace("px", "")
-
-        const left = (parseFloat(currentLeft) || 0) + event.deltaRect.left
-        const top = (parseFloat(currentTop) || 0) + event.deltaRect.top
-        
-        if(event.rect.width <= 200) return 
-        Object.assign(event.target.style, {
-          width: `${event.rect.width}px`,
-          height: `${event.rect.height}px`,
-          top: `${top}px`,
-          left: `${left}px`,
-        })
-      }
-    }
   })
   .draggable({
-    listeners: {
-      move (event) {
-        const currentTop = window.getComputedStyle(event.target).top.replace("px", "")
-        const currentLeft = window.getComputedStyle(event.target).left.replace("px", "")
-        const top = parseInt(currentTop) + event.dy
-        const left = parseInt(currentLeft) + event.dx
-        
-        event.target.style.top = `${top}px`
-        event.target.style.left = `${left}px`
-      },
-    }
+    listeners: { move: dragMoveListener },
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ]
   })
+
+
+  function dragMoveListener (event) {
+    var target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+  
+    // translate the element
+    target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+  
+    // update the posiion attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
+  }
